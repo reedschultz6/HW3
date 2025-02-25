cabbf2ef0a17f475bc3a26c036024305715f11ab
Reed Schultz <reed.schultz@kellogg.northwestern.edu>
2025-02-25 02:19:15 +0000

    Progress

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -6,20 +6,7 @@ class PlacesController < ApplicationController
     render :template => "places/index"
 end
 def show
-  @places = Place.find_by({"id" => params["id"]})
-  @places = Place.where({"place_id" => @place["id"]})
+    @places = Place.find_by(id: params[:id])
+    @postss = Post.where(place_id: @place.id)  
+  end
 end
-
-def new
-end
-
-def create
-
-  @places = Place.new
-  @places["name"] = params["name"]
-    @places["city"] = params["city"]
-    @places["state"] = params["state"]
-    @places.save
-    redirect_to "/places"
-end
-end 
